ef829016ba05774670e9ea9fad7be01464ed5229
andersonlouzada <anderson.s.louzada@gmail.com>
2023-01-24 17:45:02 -0300

    corrige requisito 4

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
     return (
       <main>
         <Header />
-        <SolarSystem text="Planetas" />
+        <SolarSystem />
       </main>
     );
   }
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Title from './Title';
 
 class SolarSystem extends React.Component {
   render() {
-    const { text } = this.props;
     return (
       <div data-testid="solar-system">
-        <Title headline={ text } />
+        <Title headline="Planetas" />
       </div>
     );
   }
 }
 
 export default SolarSystem;
-
-SolarSystem.propTypes = {
-  text: PropTypes.string.isRequired,
-};
